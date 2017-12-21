76558b30ba509283bb876766c5be5dc3f069bdea
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2017-12-21 18:28:55 +0530

    Initial Commit

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  $("#my-file").click(function () {
+    $("input[type='file']").trigger('click');
+  });
+
+  $('input[type="file"]').on('change', function() {
+    var val = $(this).val();
+    if (val) {
+      $("#submit").removeAttr('disabled');
+    }
+    val = val.replace('C:\\fakepath\\', '&nbsp;');
+    val = val + '&nbsp;'
+    $(this).siblings('span').html(val);
+  });
+</script>
